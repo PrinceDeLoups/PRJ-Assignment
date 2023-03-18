bc7b7c3772759e32e30886b95f49ef78b0da8542
PrinceDeLoups <TuanNAHE170268@fpt.edu.vn>
2023-03-18 11:24:11 +0700

    Update LecturerController.java

--- PRJ_Assignment/src/java/controller/lecturer/LecturerController.java
+++ PRJ_Assignment/src/java/controller/lecturer/LecturerController.java
@@ -29,7 +29,6 @@ public class LecturerController extends BaseRequiredAuthenticatedController {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
         int a = (int) request.getSession().getAttribute("id");
         UserDBContext udb = new UserDBContext();
         Account acc = udb.getUser(a);
